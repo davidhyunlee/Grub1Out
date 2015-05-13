58a8e7a08c072368b3ec238d0b0da3dfce455f72
wesr7 <jwesleyrodgers@gmail.com>
2015-05-13 12:21:40 -0700

    all authentication working but instagram

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -31,18 +31,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     provides_callback_for provider
   end
 
-  def google_oauth2
+  # def google_oauth2
 
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+  #   @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
+  #   if @user.persisted?
+  #     flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+  #     sign_in_and_redirect @user, :event => :authentication
+  #   else
+  #     session["devise.google_data"] = request.env["omniauth.auth"]
+  #     redirect_to new_user_registration_url
+  #   end
+  # end
 
   # def after_sign_in_path_for(resource)
   #   if resource.email_verified?
