5d32a033d90fff6cb012223887256bd0d7ddb8f8
wesr7 <jwesleyrodgers@gmail.com>
2015-05-14 15:46:59 -0700

    deleted validations from review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ActiveRecord::Base
-  validates :body, presence: true, length: { minimum:, maximum: }
+
   belongs_to :business
   belongs_to :user
-  validates :body, presence: true
-  validates :title, presence: true, length: { maximum: 70 }
+
 end
